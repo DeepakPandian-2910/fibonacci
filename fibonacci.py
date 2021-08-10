8ab88659f859360cc321cc4cf9fbe6c6c84ffe96
DeepakPandian-2910 <88448308+DeepakPandian-2910@users.noreply.github.com>
2021-08-10 23:18:30 +0530

    created fibonacci.py
    this file prints the fibonacci element

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+n = int(input("enter the nth term: "))
+def fibonacci(n):
+    if(n < 1):
+        print("incorrect input")
+    elif(n == 0):
+        return 0
+    elif(n == 1 or n == 2):
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(n))
